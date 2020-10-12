1f80bf0252ee5af148ff2d63469a2e9cc4f79635
mdrewry <mark@DESKTOP-D5NJ26V.localdomain>
2020-10-12 19:35:04 -0400

    -created some basic routes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name='dashboard'),
+    path('addEntry', views.addEntry, name='addEntry'),
+    path('feed', views.feed, name='feed'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, HttpResponse
 from dashboard.models import Hike
 
 # Create your views here.
-def index(request):
+def dashboard(request):
     return render(request, 'dashboard.html', {'hikes': Hike.objects.all()})
+
+def addEntry(request):
+    return render(request, 'addEntry.html');
+
+def feed(request):
+    return render(request, 'feed.html');
