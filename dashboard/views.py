34673189ce293e15db519f459d3543aff14ce7a3
Andrew Gil <34136362+andrx-g@users.noreply.github.com>
2020-10-16 03:14:35 -0400

    entry created, image support added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from dashboard.models import Hike
 
 # Create your views here.
@@ -6,10 +6,14 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 def addEntry(request):
-    return render(request, 'addEntry.html');
+    if(request.POST):
+        print(request.POST.get("image"))
+        Hike.objects.createHike(request.POST.get("name"),request.POST.get("latitude"),request.POST.get("longitude"),request.POST.get("startDate"),request.POST.get("endDate"),request.POST.get("miles"),request.POST.get("elevationGain"),request.POST.get("elevationLoss"),request.POST.get("description"),False,request.FILES)
+        return HttpResponseRedirect('/dashboard')
+    return render(request, 'addEntry.html')
 
 def feed(request):
-    return render(request, 'feed.html' , {'hikes': Hike.objects.all()});
+    return render(request, 'feed.html' , {'hikes': Hike.objects.all()})
 
 def viewEntry(request,id):
     return render(request,'viewEntry.html',{'hike':Hike.objects.get(pk=id)})
