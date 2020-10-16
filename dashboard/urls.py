aa63733645cd6d5a1de4c3d2a0520ff42005bda5
Nickolas_Phen <nickolas.phen@gmail.com>
2020-10-15 20:26:00 -0400

    Dynamic Pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('addEntry', views.addEntry, name='addEntry'),
     path('feed', views.feed, name='feed'),
+    path('feed/<int:id>/',views.viewEntry,name='viewEntry'),
 ]
