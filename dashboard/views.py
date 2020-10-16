82c0ca5ca891ea5f7bc4e39301c5deb7d35fe3e3
Andrew Gil <34136362+andrx-g@users.noreply.github.com>
2020-10-16 17:57:27 -0400

    finished

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from dashboard.models import Hike
-
+from .forms import HikeForm
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard.html')
 
 def addEntry(request):
-    if(request.POST):
-        print(request.POST.get("image"))
-        Hike.objects.createHike(request.POST.get("name"),request.POST.get("latitude"),request.POST.get("longitude"),request.POST.get("startDate"),request.POST.get("endDate"),request.POST.get("miles"),request.POST.get("elevationGain"),request.POST.get("elevationLoss"),request.POST.get("description"),False,request.FILES)
-        return HttpResponseRedirect('/dashboard')
-    return render(request, 'addEntry.html')
+    if(request.method == "POST"):
+        form = HikeForm(request.POST, request.FILES)
+        if(form.is_valid()):
+            Hike.objects.createHike(request.POST.get("name"),request.POST.get("latitude"),request.POST.get("longitude"),request.POST.get("startDate"),request.POST.get("endDate"),request.POST.get("miles"),request.POST.get("elevationGain"),request.POST.get("elevationLoss"),request.POST.get("description"),False,request.FILES['image'])
+            return HttpResponseRedirect('')
+    else:
+        form = HikeForm()
+    return render(request, 'addEntry.html', {"form" : form})
 
 def feed(request):
     return render(request, 'feed.html' , {'hikes': Hike.objects.all()})
