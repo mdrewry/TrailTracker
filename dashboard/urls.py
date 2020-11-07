cfcd9aadbb1f4f6403c8fee9ce17555204178496
Nickolas_Phen <nickolas.phen@gmail.com>
2020-11-07 04:23:18 -0500

    Added a "conceptual" delete, url is the delete/{{id}}, need to possibly make redirect page to fix as well as determining location

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('addEntry', views.addEntry, name='addEntry'),
     path('<int:id>',views.viewEntry,name='viewEntry'),
     path('edit/<int:id>',views.editEntry,name='editEntry'),
+    path('delete/<int:id>',views.deleteEntry,name='deleteEntry'),
 ]
