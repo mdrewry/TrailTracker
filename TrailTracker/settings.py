c3436a27bf0244d8cbf3c2d96a0c1523403c9765
Rohan <rohan.samanta@hotmail.com>
2020-11-18 23:31:09 -0500

    added css to forms

--- TrailTracker/settings.py
+++ TrailTracker/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
